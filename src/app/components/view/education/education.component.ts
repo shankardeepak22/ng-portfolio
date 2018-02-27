6114c2a1cc40a6e151a2c8b83198a270889e0b72
Deepak <shankar.deepak22@gmail.com>
2018-02-27 07:45:28 -0500

    feat: education added with rit nad ait sub components

---
+++ src/app/components/view/education/education.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-education',
+  templateUrl: './education.component.html',
+  styleUrls: ['./education.component.css']
+})
+export class EducationComponent implements OnInit {
+  position = 'below';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
